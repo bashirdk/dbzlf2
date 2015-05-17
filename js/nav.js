8be02a4b06dc64685be12074f4a8fa0772640cc3
bashirdk <bashscrazy_88@hotmail.com>
2015-05-17 12:23:20 -0400

    major updates

---
+++ js/nav.js
@@ -0,0 +1,55 @@
+$scope.isActive = function (viewLocation) {
+     var active = (viewLocation === $location.path());
+     return active;
+};
+
+
+angular.module('link', []).
+  directive('activeLink', ['$location', function(location) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs, controller) {
+        var clazz = attrs.activeLink;
+        var path = attrs.href;
+        path = path.substring(1); //hack because path does not return including hashbang
+        scope.location = location;
+        scope.$watch('location.path()', function(newPath) {
+          if (path === newPath) {
+            element.addClass(clazz);
+          } else {
+            element.removeClass(clazz);
+          }
+        });
+      }
+    };
+  }]);
+ 
+
+ (function () {
+    angular.module('autoActive', [])
+        .directive('autoActive', ['$location', function ($location) {
+        return {
+            restrict: 'A',
+            scope: false,
+            link: function (scope, element) {
+                function setActive() {
+                    var path = $location.path();
+                    if (path) {
+                        angular.forEach(element.find('li'), function (li) {
+                            var anchor = li.querySelector('a');
+                            if (anchor.href.match('#' + path + '(?=\\?|$)')) {
+                                angular.element(li).addClass('active');
+                            } else {
+                                angular.element(li).removeClass('active');
+                            }
+                        });
+                    }
+                }
+
+                setActive();
+
+                scope.$on('$locationChangeSuccess', setActive);
+            }
+        }
+    }]);
+}());
